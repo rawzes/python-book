83e9c77f60d4b0afae11855e56e70ebe6040ece9
rawzes <rawzes@gmail.com>
2020-10-02 20:11:47 +0300

    Added strings.py

---
+++ types-operations/strings.py
@@ -0,0 +1,16 @@
+
+s = 'test'
+print(s[:]) # print whole string
+print(s[0:-1]) # print tes
+print(s[1]) # e
+print(s + "xyz") # concatination testxyz
+print("A"*90)
+#s[0] = "s"  #str' object does not support item assignment
+S = "strawberry"
+L = list(S)
+print(L)
+L[0] = 'Z'
+print(''.join(L))  # Ztrawberry
+s = ''.join(L)
+s = s.replace('rr', 'rrr')
+print(s)
