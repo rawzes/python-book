afd0301e3b0e579495c659e38c10005b4a4383da
Raman Razumeika <rawzes@gmail.com>
2021-11-18 23:16:47 +0300

    working with win32

---
+++ types-operations/chapter8/how_to_create_dict.py
@@ -0,0 +1,18 @@
+# 1
+
+d1 = {'name': 'Roma', 'age': 30}
+
+# 2
+d2 = {}
+d2['name'] = 'Roma'
+d2['age'] = 30
+# 3
+d3 = dict(name='Roma', age=30)
+
+# 4
+d4 = dict([('name', 'Roma'), ('age', 30)])
+
+print(d1)
+print(d2)
+print(d3)
+print(d4)
